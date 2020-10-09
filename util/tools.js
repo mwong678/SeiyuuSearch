fd49ea703cf9ec798e1e2cc3e62ad3b4008fa6c1
Matthew Wong <matthew.wong@wisc.edu>
2020-10-09 02:24:14 -0700

    First commit

--- util/tools.js
+++ util/tools.js
@@ -2,7 +2,19 @@ const levenshtein = require('js-levenshtein');
 
 function sort(query, array){
     return array.sort(function(a, b){
-        return levenshtein(a.name, query) - levenshtein(b.name, query);
+       if (a.name.indexOf(query) > -1){
+            if (b.name.indexOf(query) > -1){
+                return levenshtein(a.name, query) - levenshtein(b.name, query);
+            }else{
+                return -1;
+            }
+       }else{
+            if (b.name.indexOf(query) == -1){
+                return levenshtein(a.name, query) - levenshtein(b.name, query);
+            }else{
+                return 1;
+            }
+       }
     });
 }
 
